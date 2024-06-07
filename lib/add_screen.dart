309952699276255aaa9b23a82b4d69bbd95d0a7c
Anup Jana <anup@Anups-Mac-mini.local>
2024-06-07 23:29:18 +0530

    ppp

--- lib/add_screen.dart
+++ lib/add_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todoapp/contactModel.dart';
 
 class App extends StatefulWidget {
   const App({super.key});
@@ -9,9 +10,21 @@ class App extends StatefulWidget {
 
 class _AppState extends State<App> {
   final TextEditingController _nameTEController = TextEditingController();
-  final TextEditingController _ContactController = TextEditingController();
+  final TextEditingController _contactController = TextEditingController();
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  // List contactList = [];
+  List<Map<String, String>> contactList = [];
+
+  _addcontact() {
+    contactList.add(
+        {'name': _nameTEController.text, 'number': _contactController.text});
+    setState(() {});
+
+    _nameTEController.clear();
+    _contactController.clear();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,7 +56,8 @@ class _AppState extends State<App> {
                 ),
                 const SizedBox(height: 16),
                 TextFormField(
-                  controller: _ContactController,
+                  controller: _contactController,
+                  keyboardType: TextInputType.number,
                   autovalidateMode: AutovalidateMode.onUserInteraction,
                   decoration: const InputDecoration(
                       hintText: 'Number', labelText: 'Number'),
@@ -58,16 +72,116 @@ class _AppState extends State<App> {
                 ElevatedButton(
                   onPressed: () {
                     if (_formKey.currentState!.validate()) {
-                      //  _addProduct();
+                      _addcontact();
                     }
                   },
                   child: const Text('Add'),
                 ),
+                const SizedBox(height: 16),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: contactList.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: Padding(
+                          padding:
+                              const EdgeInsets.only(left: 8.00, right: 8.00),
+                          child: Container(
+                            height: 100,
+                            child: Row(
+                              children: [
+                                const Expanded(
+                                  flex: 2,
+                                  child: Icon(Icons.person),
+                                ),
+                                Expanded(
+                                  flex: 8,
+                                  child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(' ${contactList[index]["name"]}'),
+                                        Text(
+                                            ' ${contactList[index]["number"]}'),
+                                      ]),
+                                ),
+                                Expanded(
+                                    flex: 2,
+                                    child: IconButton(
+                                      icon: const Icon(Icons.phone),
+                                      onPressed: () {
+                                        showDialog(
+                                          context: context,
+                                          builder: (context) {
+                                            return AlertDialog(
+                                              title: const Text('Confirmation'),
+                                              content: const Text(
+                                                  'Are you sure for  delete ?'),
+                                              actions: [
+                                                IconButton(
+                                                  icon:
+                                                      const Icon(Icons.cancel),
+                                                  onPressed: () {
+                                                    Navigator.pop(context);
+                                                  },
+                                                ),
+                                                IconButton(
+                                                  icon:
+                                                      const Icon(Icons.delete),
+                                                  onPressed: () {
+                                                    // _deleteProduct(productId);
+                                                    Navigator.pop(context);
+                                                  },
+                                                ),
+                                              ],
+                                            );
+                                          },
+                                        );
+                                      },
+                                    )),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                      // return ListTile(
+                      //   title: Icon(Icons.phone),
+                      //   subtitle: Wrap(
+                      //     spacing: 16,
+                      //     children: [
+                      //       Text('Unit Price: ${contactList[index]["name"]}'),
+                      //       Text('Quantity : ${contactList[index]["number"]}'),
+                      //     ],
+                      //   ),
+                      //   trailing: Wrap(
+                      //     children: [
+                      //       IconButton(
+                      //         icon: const Icon(Icons.phone),
+                      //         onPressed: () {
+                      //           // _showDeleteConfirmationDialog(product.id!);
+                      //         },
+                      //       ),
+                      //     ],
+                      //   ),
+                      // );
+                    },
+                    // separatorBuilder: (_, __) => const Divider(),
+                  ),
+                )
               ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _nameTEController.dispose();
+    _contactController.dispose();
+    super.dispose();
   }
 }
---
+++ lib/contactModel.dart
@@ -0,0 +1,8 @@
+class ContactModel {
+  String? contactName;
+  String? contactNumber;
+  ContactModel(
+    this.contactName,
+    this.contactNumber,
+  );
+}
